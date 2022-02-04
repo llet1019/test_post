02e54816d4174c81d36bd528c630f7ab6b91291b
llet1019 <llet1019@gmail.com>
2022-02-04 15:49:40 +0900

    Delete SECRET_KEY

--- portprj/settings.py
+++ portprj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8sa^2p5(z1u%yq*_%k=(r02s&ugc@5p_r-*b&y*k1$-s=#$u7s'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
